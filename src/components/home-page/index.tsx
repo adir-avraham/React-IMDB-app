e6ca255535427fafafee580c7737e0c61e99bc69
adir-avraham <adir.avraham@gmail.com>
2019-09-29 20:28:58 +0300

    add-validation

--- src/components/home-page/index.tsx
+++ src/components/home-page/index.tsx
@@ -11,7 +11,7 @@ export default class HomePage extends React.Component<any, any> {
         return (
             <div>
                 <h1>HOME PAGE</h1>
-                <img src="http://www.marketmentor.co.in/Website%20in%20Progress%20%20%20_files/400_F_14974071_5hnvfUTFafvwRje7d04d87fMOiXQMCiI.jpg"/>
+                <img src="http://www.marketmentor.co.in/Website%20in%20Progress%20%20%20_files/400_F_14974071_5hnvfUTFafvwRje7d04d87fMOiXQMCiI.jpg" alt="not available"/>
             </div>
         )
     }
--- src/components/movie/index.tsx
+++ src/components/movie/index.tsx
@@ -3,15 +3,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 export default class Movie extends React.Component<any, any> {
-
-
+    
+    
     render() {
         const {Title, Year, imdbID, Type, Poster} = this.props
+        const defaultPicture = "https://images.immediate.co.uk/production/volatile/sites/3/2017/11/imagenotavailable1-39de324.png?quality=90&resize=620,413"
+        let src = null;
+        if (Poster === "N/A") {
+            src = defaultPicture
+        } else {
+        src = Poster
+        }
+   
+
         return (
       
                 
-                <div className="card" style={{width: "18rem"}}  >
-                    <img src={Poster} className="card-img-top" alt="image not available"/>
+                <div className="card mb-3" style={{width: "18rem"}}  >
+                    <img src={src} className="card-img-top" alt="not available"/>
                         <div className="card-body">
                             <h5 className="card-title">{Title}</h5>
                         </div>
