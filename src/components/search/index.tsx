6c65d4d2eaeeb61bdd748c5402b475bebe21a4c1
adir-avraham <adir.avraham@gmail.com>
2019-10-01 13:11:40 +0300

    styling-form

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -1,5 +1,7 @@
+import Button from '@material-ui/core/Button';
 import React from 'react';
-
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
 
 
 export default class MovieSearch extends React.Component<any, any> {
@@ -9,26 +11,35 @@ export default class MovieSearch extends React.Component<any, any> {
         }
     
     render() {
-        
+    
         const {searchValue, getMoviesByName} = this.props;
 
         return (
-            <div>
-            
-            Search movie: 
-            <br/>
-            
-            <input value={searchValue} onChange={(e)=>{
+            <div>           
+            <Typography variant="h5" align="center" color="inherit" paragraph>
+            SEARCH MOVIE
+            </Typography>
+
+            <TextField value={searchValue} onChange={(e)=>{
             const searchValue = e.target.value
             this.setState({searchValue})
-            }}/>
-            
-            <button onClick={()=>{
-            getMoviesByName(this.state.searchValue)
-              
-            }}>SEARCH</button>
-            
+            }}
+            label="Movie title"
+            style={{marginLeft: "2px", marginRight: "2px", marginTop: "4px"}}
+            margin="dense"
+            variant="outlined"/>
+
+            <Button variant="contained" color="primary" style={{ margin:"5.5px"}} onClick={()=>{
+            getMoviesByName(this.state.searchValue)}}>
+            SEARCH </Button>          
             </div>
         )
     }
 }
+
+
+
+
+
+
+
