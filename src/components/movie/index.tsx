7f8615915f7ebf75028e0627d16f009bd8273d82
adir-avraham <adir.avraham@gmail.com>
2019-10-01 12:03:05 +0300

    styling-cards

--- src/components/movie/index.tsx
+++ src/components/movie/index.tsx
@@ -1,40 +1,51 @@
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 
 export default class Movie extends React.Component<any, any> {
-    
-    
+
+
     render() {
-        const {Title, Year, imdbID, Type, Poster} = this.props
+        const { Title, Year, imdbID, Type, Poster } = this.props
         const defaultPicture = "https://images.immediate.co.uk/production/volatile/sites/3/2017/11/imagenotavailable1-39de324.png?quality=90&resize=620,413"
         let src = null;
         if (Poster === "N/A") {
             src = defaultPicture
         } else {
-        src = Poster
+            src = Poster
         }
-   
 
         return (
-      
-                
-                <div className="card mb-3" style={{width: "18rem"}}  >
-                    <img src={src} className="card-img-top" alt="not available"/>
-                        <div className="card-body">
-                            <h5 className="card-title">{Title}</h5>
-                        </div>
-                        <ul className="list-group list-group-flush">
-                            <li className="list-group-item">Year: {Year}</li>
-                            <li className="list-group-item">Type: {Type}</li>
-                            <li className="list-group-item">imdb-ID: {imdbID}</li>
-                        </ul>
-                </div>
-
-              
-
-
-
-                )
-            }
-        }
+              <Grid item xs={12} sm={6} md={4}>
+                <Card style={{ height: '100%',  display: 'flex',  flexDirection: 'column'}}>
+                  <CardMedia
+                    style={{paddingTop: '56.25%'}}
+                    image={src}
+                    title="Image title"/>
+                  <CardContent style={{flexGrow: 1}}>
+                    <Typography gutterBottom variant="h5" component="h2">{Title}</Typography>
+                    <Typography>Year: {Year}</Typography>
+                    <Typography>Type: {Type}</Typography>
+                    <Typography>imdbID: {imdbID}</Typography>
+                  </CardContent>
+                  <CardActions>
+                  <a href={`https://www.imdb.com/title/${imdbID}/`} target="_blanck">
+                    <Button size="small" color="primary">IMDB</Button>
+                  </a>
+                  <a href={`https://www.youtube.com/results?search_query=${Title}`} target="_blanck">
+                    <Button size="small" color="primary">YouTube</Button>
+                  </a>
+                  </CardActions>
+                </Card>
+              </Grid>
+        )
+    }
+}
--- src/components/movies-list/index.tsx
+++ src/components/movies-list/index.tsx
@@ -1,5 +1,8 @@
-import  React from 'react';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
 import Movie from '../movie'
+import  React from 'react';
+
 
 
 export default class MoviesList extends React.Component<any, any> {
@@ -8,11 +11,13 @@ export default class MoviesList extends React.Component<any, any> {
         const {movies} = this.props
 
         return (
-            <div>
-              
-              {movies.map((movie:any)=> <Movie  key={movie.imdbID}  {...movie}/>)}  
-            </div>
+          
+        <Container style={{paddingTop: "16px", paddingBottom: "16px" }} maxWidth="md">
+            <Grid container spacing={4}>
+                {movies.map((movie:any)=>   <Movie  key={movie.imdbID}  {...movie}/> )} 
+
+            </Grid>
+        </Container>
         )
     }
-
 }
